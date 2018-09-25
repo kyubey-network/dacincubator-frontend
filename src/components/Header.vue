7b071d80fdf7fc23e0bb0cf305c6a82d04698298
Frank Wei <frank@frankwei.xyz>
2018-09-25 17:23:06 +0800

    feat: new `build-deploy` cmd

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
           <div class="header">
-            <el-badge value="Beta on Kylin" class="item">
+            <el-badge value="Beta" class="item">
                 <h1 class="title"> Kyubey IBO </h1>
             </el-badge >
             <h2 class="subtitle"> 请认准 Kyubey 官方域名以防损失 </h2>
