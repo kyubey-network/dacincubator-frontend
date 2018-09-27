6495dbd0f072e22757a30f5d3936c5df000656af
Frank Wei <frank@frankwei.xyz>
2018-09-27 16:06:18 +0800

    chore: disable `TradeOld` js code, i don't want to be bothered by linter

--- src/views/TradeOld.vue
+++ src/views/TradeOld.vue
@@ -16,87 +16,87 @@
     </el-card>
 </template>
 
-<script>
-import { Notification } from 'element-ui';
-import { TradeToken } from '../components';
-import { mapState, mapActions, mapMutations, mapGetters } from 'vuex';
-import { network } from '../config';
+// <!--script>
+// import { Notification } from 'element-ui';
+// import { TradeToken } from '../components';
+// import { mapState, mapGetters } from 'vuex';
+// import { network } from '../config';
 
-const requiredFields = { accounts: [network] };
+// const requiredFields = { accounts: [network] };
 
-export default {
-  name: 'Trade',
-  data: () => ({}),
-  components: {
-    TradeToken,
-  },
-  methods: {
-    async buy() {
-      const { eos, account } = this;
-      const amountOfEOS = Number(prompt('请输入你要购少 KBY 等值的 EOS？（格式1.0000)'));
-      const memo = 'buy';
-      try {
-        await eos.transfer(
-          account.name,
-          'dacincubator',
-          `${amountOfEOS.toFixed(4)} EOS`,
-          `${memo}`,
-          {
-            authorization: [`${account.name}@${account.authority}`],
-          },
-        );
-        Notification.success({
-          title: '购买成功',
-          message: '请稍后查询你的余额',
-        });
-      } catch (error) {
-        Notification.error({
-          title: '购买失败',
-          message: `原因: ${error.message}`,
-          duration: 0,
-        });
-      }
-    },
-    async eosTokenTransfer({
-      tokenContract, to, amount, memo, account,
-    }) {
-      const { eos } = this;
-      const contract = await eos.contract(tokenContract);
-      return contract.transfer(account.name, to, amount, memo, {
-        authorization: [`${account.name}@${account.authority}`],
-      });
-    },
-    async sell() {
-      const { eos, account } = this;
-      const amountOfKBY = Number(prompt('请输入你出售多少个 KBY？ （格式1.0000)')).toFixed(4);
-      const memo = 'sell';
-      try {
-        await this.eosTokenTransfer({
-          tokenContract: 'dacincubator',
-          to: 'dacincubator',
-          amount: `${amountOfKBY} KBY`,
-          memo,
-          account,
-        });
-        Notification.success({
-          title: '购买成功',
-          message: '请稍后查询你的余额',
-        });
-      } catch (error) {
-        Notification.error({
-          title: '购买失败',
-          message: `原因: ${error.message}`,
-          duration: 0,
-        });
-      }
-    },
-  },
-  computed: {
-    ...mapState(['identity', 'scatter', 'eos', 'account']),
-    ...mapGetters(['account']),
-  },
-};
-</script>
+// export default {
+//   name: 'Trade',
+//   data: () => ({}),
+//   components: {
+//     TradeToken,
+//   },
+//   methods: {
+//     async buy() {
+//       const { eos, account } = this;
+//       const amountOfEOS = Number(prompt('请输入你要购少 KBY 等值的 EOS？（格式1.0000)'));
+//       const memo = 'buy';
+//       try {
+//         await eos.transfer(
+//           account.name,
+//           'dacincubator',
+//           `${amountOfEOS.toFixed(4)} EOS`,
+//           `${memo}`,
+//           {
+//             authorization: [`${account.name}@${account.authority}`],
+//           },
+//         );
+//         Notification.success({
+//           title: '购买成功',
+//           message: '请稍后查询你的余额',
+//         });
+//       } catch (error) {
+//         Notification.error({
+//           title: '购买失败',
+//           message: `原因: ${error.message}`,
+//           duration: 0,
+//         });
+//       }
+//     },
+//     async eosTokenTransfer({
+//       tokenContract, to, amount, memo, account,
+//     }) {
+//       const { eos } = this;
+//       const contract = await eos.contract(tokenContract);
+//       return contract.transfer(account.name, to, amount, memo, {
+//         authorization: [`${account.name}@${account.authority}`],
+//       });
+//     },
+//     async sell() {
+//       const { account } = this;
+//       const amountOfKBY = Number(prompt('请输入你出售多少个 KBY？ （格式1.0000)')).toFixed(4);
+//       const memo = 'sell';
+//       try {
+//         await this.eosTokenTransfer({
+//           tokenContract: 'dacincubator',
+//           to: 'dacincubator',
+//           amount: `${amountOfKBY} KBY`,
+//           memo,
+//           account,
+//         });
+//         Notification.success({
+//           title: '购买成功',
+//           message: '请稍后查询你的余额',
+//         });
+//       } catch (error) {
+//         Notification.error({
+//           title: '购买失败',
+//           message: `原因: ${error.message}`,
+//           duration: 0,
+//         });
+//       }
+//     },
+//   },
+//   computed: {
+//     ...mapState(['identity', 'scatter', 'eos', 'account']),
+//     ...mapGetters(['account']),
+//   },
+// };
+// </script>
 
 <style scoped>
-</style>
+</style >
