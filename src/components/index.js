59d3ab4352b6a7652053d9a8c43c74bb8a79e0a0
Frank Wei <frank@frankwei.xyz>
2018-09-27 15:54:21 +0800

    refactor: remove `TradeTokenOld`

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
-// export { default as Dashboard } from './Dashboard';
-export { default as Footer } from './Footer';
-export { default as Header } from './Header';
-export { default as TradeToken } from './TradeTokenOld';
-export { default as CrowdSale } from './CrowdSaleToken';
-export { default as CrowdSaleStat } from './CrowdSaleStat';
-export { default as LeftPanel } from './LeftPanel';
+export { default as Footer } from './Footer.vue';
+export { default as Header } from './Header.vue';
+export { default as CrowdSale } from './CrowdSaleToken.vue';
+export { default as CrowdSaleStat } from './CrowdSaleStat.vue';
+export { default as LeftPanel } from './LeftPanel/index.vue';
--- src/router.js
+++ src/router.js
@@ -21,11 +21,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-    {
-      path: '/dev',
-      name: 'dev-trade-board',
-      component: () => import(/* webpackChunkName: "about" */ './components/TradeTokenOld.vue'),
-    },
+    }
   ],
 });
