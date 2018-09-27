dddcba9a3183ac407e140d6afb43768730e79032
Frank Wei <frank@frankwei.xyz>
2018-09-27 15:56:17 +0800

    chore: delete demo for `route level code-splitting`

--- src/router.js
+++ src/router.js
@@ -13,14 +13,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Trade,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     }
   ],
 });
